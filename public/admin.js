3fc0c3d92a01f72675731d834137ac40ceec0f2e
astonelv <158239756+astonelv@users.noreply.github.com>
2024-04-11 00:56:50 -0700

    update

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,18 @@
 
 // Your Code Here
+async function updateBook() {
+  let response = await fetch('http://localhost:3001/updatBook', {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        'id': 3,
+        'title': 'Legends of Arathrae',
+    }),
+  }); 
+  let updateBook = await response.json();
+console.log(updateBook)
+}
+
+updateBook()
